79f6677e1c924cab292f101557fed320ddd9d04c
ben <nlp2ct.ben@gmail.com>
2016-09-28 03:59:13 +0800

    bug fix fastalign

--- python/groundHog/create.sh
+++ python/groundHog/create.sh
@@ -11,15 +11,15 @@ OUTPUT_NAME=$OUTPUT_DIR/train
 
 mkdir -p $OUTPUT_DIR
 
-python preprocess.py $INPUT_SOURCE -d $OUTPUT_NAME.source.wordid.pkl -v 50000 -b $OUTPUT_DIR/binarized_text.source.pkl -p
-python preprocess.py $INPUT_TARGET -d $OUTPUT_NAME.target.wordid.pkl -v 50000 -b $OUTPUT_DIR/binarized_text.target.pkl -p
+python ~/program/python/groundHog/preprocess.py $INPUT_SOURCE -d $OUTPUT_NAME.source.wordid.pkl -v 50000 -b $OUTPUT_DIR/binarized_text.source.pkl -p
+python ~/program/python/groundHog/preprocess.py $INPUT_TARGET -d $OUTPUT_NAME.target.wordid.pkl -v 50000 -b $OUTPUT_DIR/binarized_text.target.pkl -p
 
-python invert-dict.py $OUTPUT_NAME.source.wordid.pkl $OUTPUT_NAME.source.idword.pkl
-python invert-dict.py $OUTPUT_NAME.target.wordid.pkl $OUTPUT_NAME.target.idword.pkl
+python ~/program/python/groundHog/invert-dict.py $OUTPUT_NAME.source.wordid.pkl $OUTPUT_NAME.source.idword.pkl
+python ~/program/python/groundHog/invert-dict.py $OUTPUT_NAME.target.wordid.pkl $OUTPUT_NAME.target.idword.pkl
 
-python convert-pkl2hdf5.py $OUTPUT_DIR/binarized_text.source.pkl $OUTPUT_DIR/binarized_text.source.h5
-python convert-pkl2hdf5.py $OUTPUT_DIR/binarized_text.target.pkl $OUTPUT_DIR/binarized_text.target.h5
+python ~/program/python/groundHog/convert-pkl2hdf5.py $OUTPUT_DIR/binarized_text.source.pkl $OUTPUT_DIR/binarized_text.source.h5
+python ~/program/python/groundHog/convert-pkl2hdf5.py $OUTPUT_DIR/binarized_text.target.pkl $OUTPUT_DIR/binarized_text.target.h5
 
-python shuffle-hdf5.py $OUTPUT_DIR/binarized_text.source.h5 $OUTPUT_DIR/binarized_text.target.h5 $OUTPUT_DIR/binarized_text.shuffled.source.h5 $OUTPUT_DIR/binarized_text.shuffled.target.h5
+python ~/program/python/groundHog/shuffle-hdf5.py $OUTPUT_DIR/binarized_text.source.h5 $OUTPUT_DIR/binarized_text.target.h5 $OUTPUT_DIR/binarized_text.shuffled.source.h5 $OUTPUT_DIR/binarized_text.shuffled.target.h5
 
 
