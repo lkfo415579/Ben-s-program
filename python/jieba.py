31f57a00e871443fc407ec3140f37220168efb29
ben <nlp2ct.ben@gmail.com>
2016-11-26 17:21:08 +0800

    modify

--- python/jieba.py
+++ python/jieba.py
@@ -1,13 +1,14 @@
 import jieba
 import sys
 import os
+import codecs
 
-#if len(sys.argv) != 3:
-#	print 'Usage: python', sys.argv[0], 'input output'
-#	exit()
+if len(sys.argv) != 3:
+	print 'Usage: python', sys.argv[0], '[input] [output]'
+#exit()
 
-fr = open(sys.argv[1], 'r')
-fw = open(sys.argv[2], 'w')
+fr = codecs.open(sys.argv[1], 'r', encoding='utf-8')
+fw = codecs.open(sys.argv[2], 'w', encoding='utf-8')
 
 for line in fr:
 	seg_list = jieba.cut(line.strip())
