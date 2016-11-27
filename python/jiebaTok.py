a723224bf689dde15552c0c97101553861570ee6
ben <nlp2ct.ben@gmail.com>
2016-11-27 14:26:10 +0800

    fix jieba.py bug

--- python/jiebaTok.py
+++ python/jiebaTok.py
@@ -5,7 +5,7 @@ import codecs
 
 if len(sys.argv) != 3:
 	print 'Usage: python', sys.argv[0], '[input] [output]'
-#exit()
+	exit()
 
 fr = codecs.open(sys.argv[1], 'r', encoding='utf-8')
 fw = codecs.open(sys.argv[2], 'w', encoding='utf-8')
